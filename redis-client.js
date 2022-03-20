e3731c01a7fbedce6600cbd6d7b2462c34b4d93c
Nolan Kurylo <44009838+nolankurylo@users.noreply.github.com>
2022-03-19 19:06:20 -0700

    testing  docker

--- redis-client.js
+++ redis-client.js
@@ -2,16 +2,6 @@ const redis = require('redis');
 const { promisify } = require('util');
 const client = redis.createClient(process.env.REDIS_URL)
 
-client.on("connect", function () {
-    console.log("redis connected");
-    console.log(`connected ${redisClient.connected}`);
-  });
-
-  client.on("error", (err) => {
-    console.log(err);
-  });
-  
-  
 module.exports = {
     ...client,
     getAsync: promisify(client.get).bind(client),
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const redis = require('redis');
 const client = redis.createClient(process.env.REDIS_URL)
-
+const redisClient = require('./redis-client')
 app.get('/store/:key', async (req, res) => {
     const { key } = req.params
     const value = req.query 
@@ -17,6 +17,19 @@ app.get('/:key', async (req, res) => {
     const rawData = await client.get(key);
     return res.json(JSON.parse(rawData))
 })
+
+// app.get('/store/:key', async (req, res) => {
+//     const { key } = req.params
+//     const value = req.query 
+//     await redisClient.setAsync(key, JSON.stringify(value))
+//     return res.send('Success')
+// })
+
+// app.get('/:key', async (req, res) => {
+//     const { key } = req.params
+//     const rawData = await redisClient.getAsync(key)
+//     return res.json(JSON.parse(rawData))
+// })
 
 app.get('/', async (req, res) => {
     return res.send('Hello world');
