1a72863ebfe6166dcf074b7ee621378b0d1ab4ab
Nolan Kurylo <44009838+nolankurylo@users.noreply.github.com>
2022-03-19 18:03:58 -0700

    first commit

---
+++ redis-client.js
@@ -0,0 +1,20 @@
+const redis = require('redis');
+const { promisify } = require('util');
+const client = redis.createClient(process.env.REDIS_URL)
+client.connect()
+client.on("connect", function () {
+    console.log("redis connected");
+    console.log(`connected ${redisClient.connected}`);
+  });
+
+  client.on("error", (err) => {
+    console.log(err);
+  });
+  
+  
+module.exports = {
+    ...client,
+    getAsync: promisify(client.get).bind(client),
+    setAsync: promisify(client.set).bind(client),
+    keysAsync: promisify(client.keys).bind(client),
+}
